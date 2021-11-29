673c27527fc7527371af253420bd9045b6e82df4
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-11-29 14:42:25 -0800

    Add project files.

---
+++ WelcomeAcme/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WelcomeAcme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Tech Academy Accounting Systems \n");
+            Console.WriteLine("Remember, we\'re \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
