370cb22e93d84309dd2f4177c3c2ac86e0f7e3c0
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-11-30 11:10:31 -0800

    Math

---
+++ MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numA = 6;
+            int numB = 2;
+            int totalAdd = numA + numB;
+            Console.WriteLine("Add: "+totalAdd);
+            int totalSubstract = numA - numB;
+            Console.WriteLine("Substract: "+totalSubstract);
+            int totalMultiply = numA * numB;
+            Console.WriteLine("Multiply: "+ totalMultiply);
+            int totalDivide = numA / numB;
+            Console.WriteLine("Divide: " + totalDivide);
+            Console.ReadLine();
+        }
+    }
+}
