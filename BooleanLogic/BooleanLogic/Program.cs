bcbb2e0a5da461c229cbcd2d7a2e490c811e141b
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-11-30 14:30:20 -0800

    boolean

--- BooleanLogic/BooleanLogic/Program.cs
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -10,18 +10,20 @@ namespace BooleanLogic
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(true && true);
-
-            //Console.WriteLine(true || false);
-
-            //Console.WriteLine(true == true);
-
-            //Console.WriteLine(true != false);
-            
-            //XOR OPERATOR
-            //Console.WriteLine(true ^ true);
-            //Console.WriteLine(true ^ false);
-            //Console.WriteLine(false ^ false);
+            Console.WriteLine("BOOLEAN LOGIC SUBMISSION");
+            Console.WriteLine("What is your age?");
+            //Read and convert to int the age 
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?, Answer \"true\" or \"false\"");
+            //Read and convert to bool the dui 
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            //Read and convert to int the tickets 
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            //Compare if the age is over 15 years old AND not have any DUI's AND not have more than 3 speeding tickets
+            bool qualified = (age > 15) && (!dui) && (tickets<3);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
 
             Console.ReadLine();
         }
