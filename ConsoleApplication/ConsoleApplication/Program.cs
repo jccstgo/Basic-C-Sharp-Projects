19c18d807c22489aaf48a85de0b6b89cfde9a58a
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-11-30 12:16:42 -0800

    Console

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            int multiplies = input % 7;
+            Console.WriteLine("Result Multiplies: "+multiplies);
+            Console.ReadLine();
+            
+        }
+    }
+}
