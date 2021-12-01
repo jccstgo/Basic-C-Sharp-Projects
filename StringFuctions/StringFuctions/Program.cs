f9f639111284fa24518273c4505e2de11ccecba0
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-11-30 16:06:02 -0800

    Strings

---
+++ StringFuctions/StringFuctions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFuctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Carlos";
+            //string quote = "The man said, \"Hello\"";
+            //string fileName = @"\:a|";
+            //bool tf = name.Contains("s");
+            //string r = name.ToUpper();
+            //Console.WriteLine(r);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Carlos");
+            sb.Append("My lastname is Cruz");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
