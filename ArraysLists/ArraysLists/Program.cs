6523b02f64cf125cae588f9bd6248410cde5dfec
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-11-30 16:26:22 -0800

    Correction

---
+++ ArraysLists/ArraysLists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[5];
+            numArray[0] = 5;
+            numArray[1] = 10;
+            numArray[2] = 200;
+            numArray[3] = 300;
+            numArray[4] = 4000;
+
+            int[] numArray1 = new int[] { 5, 10, 200, 300, 4000 };
+         }
+    }
+}
--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -13,22 +13,30 @@ namespace Branching
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("Please enter the package weight:");
             float weight = float.Parse(Console.ReadLine());//Read the weight and convert the string to float
-            if (weight > 50) {
+            if (weight > 50)
+            {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-            Console.WriteLine("Please enter the package width:");
-            float width = float.Parse(Console.ReadLine());//Read the width and convert the string to float
-            Console.WriteLine("Please enter the package height:");
-            float height = float.Parse(Console.ReadLine());//Read the height and convert the string to float
-            Console.WriteLine("Please enter the package length:");
-            float lenght = float.Parse(Console.ReadLine());//Read the length and convert the string to float
-            float dimension = width * height * lenght;//Calculate the dimension
-            if (dimension > 50){
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+            else {
+                Console.WriteLine("Please enter the package width:");
+                float width = float.Parse(Console.ReadLine());//Read the width and convert the string to float
+                Console.WriteLine("Please enter the package height:");
+                float height = float.Parse(Console.ReadLine());//Read the height and convert the string to float
+                Console.WriteLine("Please enter the package length:");
+                float lenght = float.Parse(Console.ReadLine());//Read the length and convert the string to float
+                float dimension = width * height * lenght;//Calculate the dimension
+                if (dimension > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                { 
+                    float quote = (weight * dimension) / 100;//Calculate the quote
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote);//Print the result of the quote
+                    Console.WriteLine("Thank you!");
+                }                
             }
-            float quote = (weight * dimension) / 100;//Calculate the quote
-            Console.WriteLine("Your estimated total for shipping this package is: $"+quote);//Print the result of the quote
-            Console.WriteLine("Thank you!");
+            
             Console.ReadLine();
         }
     }
