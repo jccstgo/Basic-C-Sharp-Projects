66feb034e8a5c884eb8ebaef3eb87f0a8db08518
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-12-03 12:33:50 -0800

    names

---
+++ ParametersByName/ParametersByName/Names.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersByName
+{
+    class Names
+    {
+        public void Operation(int number_1, int number_2) 
+        {
+            int addTwo = number_1 + 2;//Take the firs number and add 2
+            
+            //Print the result of the operation in the firs number and shows the second number
+            Console.WriteLine("The first number + 2 is:" + addTwo + " and the second number is: "+number_2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParametersByName/ParametersByName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Names name = new Names();//Intantiate the Class "Names"
+            name.Operation(number_1: 3, number_2: 13);//Call the method "Operations" specifying the parameters by name
+        }
+    }
+}
