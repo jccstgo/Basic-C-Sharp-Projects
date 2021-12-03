17e0feabc85deaf76dc6e091a38a1b8abcccfcb0
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-12-03 11:48:21 -0800

    bisiesto

---
+++ MethodOptionalParameter/MethodOptionalParameter/Optional.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionalParameter
+{
+    class Optional
+    {
+        public int LeapYear(int year_1, int year_2=2022)
+        {
+            bool leap_1 = (year_1 % 4 == 0) && (!(year_1 % 100 == 0) || (year_1 % 400 == 0));//Verify if the fisrt year "year 1" is a leap year
+            bool leap_2 = (year_2 % 4 == 0) && (!(year_2 % 100 == 0) || (year_2 % 400 == 0));//Verify if the fisrt year "year 2" is a leap year
+
+            if (leap_1 && leap_2)//Return 1 if year_1 and year_2 are leap
+            {
+                return 1;
+            }
+            else if (leap_1)//Return 2 if only year_1 is leap
+            {
+                return 2;
+            }
+            else if (leap_2)//Return 3 if only year_2 is leap
+            {
+                return 3;
+            }
+            else //Return 4 if no one of the years are leap
+            {
+                return 4;
+            }
+        }
+    }
+}
---
+++ MethodOptionalParameter/MethodOptionalParameter/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Optional optional = new Optional();//Instantiate the Class "Optional"
+            
+            Console.WriteLine("This program calculates if two years is a leap year.");
+            
+            //Ask and read the first year
+            Console.Write("\n\nInput the firs year: ");
+            int year_1 = Convert.ToInt32(Console.ReadLine());//Convert the input in type integer
+
+            //Aks the user if want to introduce another year
+            Console.WriteLine("\n\nIf you want to introduce another year, write: \"Yes\"");
+            string another = Console.ReadLine().ToUpper();//Read and convert to upper case the answer 
+
+
+            if (another == "YES")//If the user wants to introduce another year, we ask for this
+            {
+                Console.WriteLine("\nInput the second year(is optional introduce it): ");
+                int year_2 = Convert.ToInt32(Console.ReadLine());//Convert the input in type integer
+                int isLeap = optional.LeapYear(year_1, year_2);//WE USE THE METHOD "LeapYear" WITH TWO PARAMETERS
+                if (isLeap == 1)//If the variable "isLeap" is 1 the year_1 and year_2 are leap
+                {
+                    Console.WriteLine("The years " + year_1 + " and " + year_2 + " are leap");
+                }
+                else if (isLeap == 2)//If the variable "isLeap" is 2 only the year_1 is leap
+                {
+                    Console.WriteLine("The year " + year_1 + " is leap and the year " + year_2 + " isn't leap");
+                }
+                else if (isLeap == 3)//If the variable "isLeap" is 3 only the year_2 is leap
+                {
+                    Console.WriteLine("The year " + year_2 + " is leap and the year " + year_1 + " isn't leap");
+                }
+                else//Else, no one of the years are leap
+                {
+                    Console.WriteLine("The year " + year_1 + " and " + year_2 + "aren't leap");
+                }
+            }
+            else //And the other hand if the user only want use a number we do 
+            {
+                optional.LeapYear(year_1);
+                int isLeap = optional.LeapYear(year_1);//WE USE THE METHOD "LeapYear" WITH ONE PARAMETERS
+                if (isLeap == 1)//If the variable "isLeap" is 1 the year_1 and year_2 are leap
+                {
+                    Console.WriteLine("The years " + year_1 + " and 2022 are leap");
+                }
+                else if (isLeap == 2)//If the variable "isLeap" is 2 only the year_1 is leap
+                {
+                    Console.WriteLine("The year " + year_1 + " is leap and the year 2022 isn't leap");
+                }
+                else if (isLeap == 3)//If the variable "isLeap" is 3 only the year_2 is leap
+                {
+                    Console.WriteLine("The year 2022 is leap and the year " + year_1 + " isn't leap");
+                }
+                else//Else, no one of the years are leap
+                {
+                    Console.WriteLine("The year " + year_1 + " and 2022 aren't leap");
+                }
+            }
+            Console.ReadLine();
+            
+
+        }
+    }
+}
