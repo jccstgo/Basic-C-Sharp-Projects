32b58837fe7dda6729263ecd5187e86d69a139fd
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-12-03 00:44:45 -0800

    Main

---
+++ MainMethod/MainMethod/Overloading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Overloading
+    {
+        //First method that recives an integer
+        public int Operation(int number) 
+        {
+            number += 2;//Add 2
+            return number;//return the result as a integer
+        }
+
+        //Second method that recives an decimal
+        public int Operation(decimal number)
+        {
+            number *= 2;//Multiply the number (decimal) by 2
+            return (int)number;//Explicit cast to int, in order to return an integer
+        }
+
+        //The third method that recives an string
+        public int Operation(string number)
+        {            
+            int tmpNumber = Convert.ToInt32(number)-2;//Converts an the string to integer an subtract 2
+            return tmpNumber;//return the result as a integer
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Overloading methodsOverloading = new Overloading();//An instance of the Class "Overloading" 
+            
+            int number_1 = 5;
+            int result_1 = methodsOverloading.Operation(number_1);//Call the method "Operation" and passes an integer
+            Console.WriteLine("The result 5+2 is: " + result_1);//Print the result
+
+            decimal number_2= 10m;
+            int result_2 = methodsOverloading.Operation(number_2);//Call the method "Operation" and passes a decimal
+            Console.WriteLine("The result 10*2 is: " + result_2);//Print the result
+
+            string number_3 = "20";
+            int result_3 = methodsOverloading.Operation(number_3);//Call the method "Operation" and passes a string
+            Console.WriteLine("The result 20-2 is: " + result_3);//Print the result
+
+            Console.ReadLine();
+        }
+    }
+}
