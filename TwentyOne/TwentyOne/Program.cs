955e9e783d39806fe8264694fbf73a0fe3c44fd3
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-12-03 12:56:00 -0800

    Dec 3

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,45 +11,17 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            int shuffleTimes = 0;
-            deck = Shuffle(deck, out shuffleTimes,3);
+            deck.Shuffle();
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " +card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine(shuffleTimes);
             Console.ReadLine();
         }
 
-        public static Deck Shuffle(Deck deck, out int shuffleTimes, int times = 1)
-        {
-            shuffleTimes = 0;
-            for (int i = 0; i < times; i++)
-            {
-                shuffleTimes++;
-                List<Card> tempList = new List<Card>();
-                Random random = new Random();
-                while (deck.Cards.Count > 0)
-                {                    
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    tempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-                }
-                deck.Cards = tempList;
-            }
-            
-            return deck;
-        }
+       
 
-        //public static Deck Shuffle(Deck deck, int times) 
-        //{
-        //    for (int i = 0; i<times; i++) 
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
