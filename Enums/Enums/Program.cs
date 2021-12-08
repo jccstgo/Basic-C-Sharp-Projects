fa9a1bb875a55993a75478ab9e9c7ea424f60dad
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-12-07 17:02:19 -0800

    Enum

--- Enums/Enums/Program.cs
+++ Enums/Enums/Program.cs
@@ -10,6 +10,32 @@ namespace Enums
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //Ask the user about the current day
+                Console.WriteLine("Input the current day of the week");
+                string currentDay = Console.ReadLine();//Read the answer (string)
+                DaysOfTheWeek dotw = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), currentDay);//Parse the "cyrrentDay" to a type DaysOfTheWeek
+                Console.WriteLine("\n\nThe current day is: "+dotw);//Prints the day of the week
+                Console.ReadLine();
+            }
+            catch (Exception ex) //If occurs an error with the parse the next code is executed
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+
+            
+        }
+        public enum DaysOfTheWeek 
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday, 
+            Saturday        
         }
     }
 }
