9f2786a1d0f9844d5dac28106bc49e6d730e4c48
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-12-08 16:53:21 -0800

    date

--- DateTime/DateTime/Program.cs
+++ DateTime/DateTime/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DateTime
+namespace DatePractice
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The current date is: "+ DateTime.Now);//Prints the current date and time 
+            
+            //Get a number from the user
+            Console.WriteLine("\n\nWrite a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Adds the number (to the hours) to the current date  
+            Console.WriteLine("in {0} hours the date will be: {1}", number,DateTime.Now.AddHours(number));
+            Console.Read();
         }
     }
 }
