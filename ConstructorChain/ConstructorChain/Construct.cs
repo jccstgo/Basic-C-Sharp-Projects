782b5af7ebfeff985ba9326f3801fede89403611
Jose Carlos Cruz Santiago <jccstgo@gmail.com>
2021-12-08 18:43:01 -0800

    Chain

---
+++ ConstructorChain/ConstructorChain/Construct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChain
+{
+    class Construct
+    {
+        //Constructor chained to the other constructur in the class, but with one parameter, the other by default is "Domingo"
+        public Construct(string element1) : this(element1, "Domingo")
+        { 
+        }
+        //Constructor of the class with two parameters
+        public Construct(string element1, string element2)
+        {
+            Console.WriteLine("The element 1 is: {0} and the element 2 is: {1}\n\n",element1, element2);
+            Console.Read();
+        }
+    }
+}
---
+++ ConstructorChain/ConstructorChain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constante = "This is an example about const variable";//Const variable of type string
+            Console.WriteLine(constante);//Print the const named "constante"
+
+            //Create a variable (List of strings) using "var"
+            var variable = new List<string>() {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes","Sabado" };
+            Construct construct1 = new Construct(variable[0]);
+
+        }
+    }
+}
